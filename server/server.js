8880319a619dbaca1e1e0f4f7405f0bd198ed164
AML <andrewmloop@gmail.com>
2021-01-02 17:11:26 -0500

    Add node cluster support

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ import querystring from 'querystring';
 import axios from 'axios';
 import { fileURLToPath } from 'url';
 import handlebars from 'express-handlebars';
+import cluster from 'cluster';
+import os from 'os';
 
 import Login from './login.js';
 import { getUser, getMoodTrack } from './logic.js';
@@ -22,166 +24,182 @@ const redirectURI = process.env.REDIRECT_URI;
 
 let accessToken = '';
 
-// Set up middleware
-const app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-// Set up templating engine
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.engine('hbs', handlebars({
-  layoutsDir: __dirname + '/views/layouts',
-  extname: 'hbs'
-}));
+  // Fork workers
+  for (let i = 0; i < os.cpus().length; i += 1) {
+    cluster.fork();
+  }
 
-// HOME PAGE
-app.get('/', (req, res) => {
-  //res.sendFile('index.html');
-  res.render('home', {layout: 'index'});
-});
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
 
-// End point when user clicks login button
-app.get('/login', Login);
+  // Set up middleware
+  const app = express();
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(express.static(__dirname + '/public'));
 
-// End point for redirect from Spotify authentication
-// from /login end point
-app.get('/callback', (req, res) => {
-  if (req.query.error) { res.send('error') }
+  // Set up templating engine
+  app.set('view engine', 'hbs');
+  app.set('views', __dirname + '/views');
+  app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs'
+  }));
 
-  const code = req.query.code || null;
+  // HOME PAGE
+  app.get('/', (req, res) => {
+    //res.sendFile('index.html');
+    res.render('home', {layout: 'index'});
+  });
 
-  axios.post('https://accounts.spotify.com/api/token', querystring.stringify({
-    grant_type: 'authorization_code',
-    code: code,
-    redirect_uri: redirectURI,
-    client_id: client,
-    client_secret: secret
-  }))
-  .catch( err => {
-    console.log(err);
-  })
-  .then( result => {
-    accessToken = result.data.access_token;
-    const tokenType = result.data.token_type;
-    const refreshToken = result.data.refresh_token;
+  // End point when user clicks login button
+  app.get('/login', Login);
 
-    Promise.all([
-      getUser(accessToken),
-      getMoodTrack(accessToken)
-    ])
-    .catch( err => console.log(err))
-    .then( results => {
-      const displayName = results[0].display_name;
-      const recentTracks = results[1].recentTracks;
-      const moodTrack = results[1].moodTrack;
-      const moodAnalysis = results[1].moodAnalysis;
-      /* console.log('display name', displayName);
-      console.log('moodtrack', moodTrack.tracks[0]);
-      console.log('album cover', moodTrack.tracks[0].album.images);
-      console.log('recent tracks', recentTracks[0].track); */
-      res.render('mood', {
-        layout: 'index',
-        display_name: displayName,
-        mood: moodAnalysis.mood,
-        mood_desc: moodAnalysis.mood_desc,
-        mood_track: moodTrack.tracks[0],
-        recent_tracks: recentTracks
+  // End point for redirect from Spotify authentication
+  // from /login end point
+  app.get('/callback', (req, res) => {
+    if (req.query.error) { res.send('error') }
+
+    const code = req.query.code || null;
+
+    axios.post('https://accounts.spotify.com/api/token', querystring.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: redirectURI,
+      client_id: client,
+      client_secret: secret
+    }))
+    .catch( err => {
+      console.log(err);
+    })
+    .then( result => {
+      accessToken = result.data.access_token;
+      const tokenType = result.data.token_type;
+      const refreshToken = result.data.refresh_token;
+
+      Promise.all([
+        getUser(accessToken),
+        getMoodTrack(accessToken)
+      ])
+      .catch( err => console.log(err))
+      .then( results => {
+        const displayName = results[0].display_name;
+        const recentTracks = results[1].recentTracks;
+        const moodTrack = results[1].moodTrack;
+        const moodAnalysis = results[1].moodAnalysis;
+        /* console.log('display name', displayName);
+        console.log('moodtrack', moodTrack.tracks[0]);
+        console.log('album cover', moodTrack.tracks[0].album.images);
+        console.log('recent tracks', recentTracks[0].track); */
+        res.render('mood', {
+          layout: 'index',
+          display_name: displayName,
+          mood: moodAnalysis.mood,
+          mood_desc: moodAnalysis.mood_desc,
+          mood_track: moodTrack.tracks[0],
+          recent_tracks: recentTracks
+        });
       });
     });
   });
-});
 
-// End point for when user clicks add to library
-// on their mood track
-app.post('/add', async (req, res) => {
-  // Song id from req params
-  const id = req.body.songId;
+  // End point for when user clicks add to library
+  // on their mood track
+  app.post('/add', async (req, res) => {
+    // Song id from req params
+    const id = req.body.songId;
 
-  await axios({
-    method: 'put',
-    url: 'https://api.spotify.com/v1/me/tracks',
-    headers: {
-      Authorization: 'Bearer ' + accessToken,
-      'Content-Type': 'application/json'
-    },
-    data: {
-      ids: [id]
-    }
-  })
-  .catch( error => {
-    console.log(error)
-    res.sendStatus(400);
-  })
-  .then( response => {
-    if (response.status === 200) {
-      res.sendStatus(200);
-    }
-  });
-});
-
-// Middleware for /search and /submit endpoints to verify
-// search token and fetch a new one if necessary
-app.use('/search', getSearchToken)
-
-
-// Fetch songs from Spotify search api
-app.post('/search', async (req, res) => {
-  const songTitle = req.body.songTitle;
-
-  await axios({
-    method: 'get',
-    url: 'https://api.spotify.com/v1/search',
-    headers: {
-      Authorization: `Bearer ${req.searchToken}`
-    },
-    params: {
-      q: songTitle,
-      type: 'track',
-      limit: 5,
-
-    }
-  })
-  .catch( error => {
-    console.log(error);
-  })
-  .then( response => {
-    res.json(response.data.tracks.items);
-  })
-});
-
-app.use('/submit', getSearchToken)
-
-// Get mood track similar to the login api endpoint
-// Uses search tracks as seeds for mood track
-app.post('/submit', async (req, res) => {
-  const id1 = req.body.song_id_1;
-  const id2 = req.body.song_id_2;
-  const id3 = req.body.song_id_3;
-  const songIds = [id1, id2, id3];
-  const searchToken = req.searchToken;
-
-  Promise.all([
-    getMoodTrackFromSearch(searchToken, songIds)
-  ])
-  .catch( error => console.log(error) )
-  .then( result => {
-    const searchTracks = result[0].searchTracks;
-    const moodTrack = result[0].moodTrack;
-    const moodAnalysis = result[0].moodAnalysis;
-
-    res.render('mood', {
-      layout: 'index',
-      mood: moodAnalysis.mood,
-      mood_desc: moodAnalysis.mood_desc,
-      mood_track: moodTrack.tracks[0],
-      search_tracks: searchTracks
+    await axios({
+      method: 'put',
+      url: 'https://api.spotify.com/v1/me/tracks',
+      headers: {
+        Authorization: 'Bearer ' + accessToken,
+        'Content-Type': 'application/json'
+      },
+      data: {
+        ids: [id]
+      }
+    })
+    .catch( error => {
+      console.log(error)
+      res.sendStatus(400);
+    })
+    .then( response => {
+      if (response.status === 200) {
+        res.sendStatus(200);
+      }
     });
   });
-});
 
-app.listen(port, () => {
-  console.log(`Mood Track running. Listening on port ${port}`);
-});
+  // Middleware for /search and /submit endpoints to verify
+  // search token and fetch a new one if necessary
+  app.use('/search', getSearchToken)
+
+
+  // Fetch songs from Spotify search api
+  app.post('/search', async (req, res) => {
+    const songTitle = req.body.songTitle;
+
+    await axios({
+      method: 'get',
+      url: 'https://api.spotify.com/v1/search',
+      headers: {
+        Authorization: `Bearer ${req.searchToken}`
+      },
+      params: {
+        q: songTitle,
+        type: 'track',
+        limit: 5,
+
+      }
+    })
+    .catch( error => {
+      console.log(error);
+    })
+    .then( response => {
+      res.json(response.data.tracks.items);
+    })
+  });
+
+  app.use('/submit', getSearchToken)
+
+  // Get mood track similar to the login api endpoint
+  // Uses search tracks as seeds for mood track
+  app.post('/submit', async (req, res) => {
+    const id1 = req.body.song_id_1;
+    const id2 = req.body.song_id_2;
+    const id3 = req.body.song_id_3;
+    const songIds = [id1, id2, id3];
+    const searchToken = req.searchToken;
+
+    Promise.all([
+      getMoodTrackFromSearch(searchToken, songIds)
+    ])
+    .catch( error => console.log(error) )
+    .then( result => {
+      const searchTracks = result[0].searchTracks;
+      const moodTrack = result[0].moodTrack;
+      const moodAnalysis = result[0].moodAnalysis;
+
+      res.render('mood', {
+        layout: 'index',
+        mood: moodAnalysis.mood,
+        mood_desc: moodAnalysis.mood_desc,
+        mood_track: moodTrack.tracks[0],
+        search_tracks: searchTracks
+      });
+    });
+  });
+
+  app.listen(port, () => {
+    console.log(`Mood Track running. Listening on port ${port}`);
+  });
+}
+
+
