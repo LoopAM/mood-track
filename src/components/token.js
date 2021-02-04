bd478218d5078e8a75325d2726fc5a6bbf6fb8b8
AML <andrewmloop@gmail.com>
2021-02-04 00:30:06 -0500

    Tweak token logic

--- src/components/token.js
+++ src/components/token.js
@@ -4,32 +4,24 @@ import axios from 'axios';
 const client = process.env.CLIENT_ID;
 const secret = process.env.SECRET;
 
-let searchToken = '';
-
 export async function getSearchToken(req, res, next) {
-  if (!searchToken) {
-    await axios({
-      method: 'post',
-      url: 'https://accounts.spotify.com/api/token',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization:
-          `Basic ${Buffer.from(client + ':' + secret).toString('base64')}`
-      },
-      params: {
-        grant_type: 'client_credentials'
-      }
-    })
-    .catch( error => {
-      console.log(error);
-    })
-    .then( response => {
-      searchToken = response.data.access_token;
-      req.searchToken = searchToken;
-      next();
-    })
-  } else {
-    req.searchToken = searchToken;
+  await axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization:
+        `Basic ${Buffer.from(client + ':' + secret).toString('base64')}`
+    },
+    params: {
+      grant_type: 'client_credentials'
+    }
+  })
+  .catch( error => {
+    console.log(error);
+  })
+  .then( response => {
+    req.searchToken = response.data.access_token;
     next();
-  }
+  });
 }
